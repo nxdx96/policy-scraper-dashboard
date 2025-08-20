7c0008d333be15f9cb09a933313d9015da3589d5
Nada Ibrahim <101666864+nxdx96@users.noreply.github.com>
2025-08-19 23:53:51 -0400

    add scraping macros functionality

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import uuid
 import os
 import random
 import threading
